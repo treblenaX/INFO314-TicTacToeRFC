ae71ca3553540d9df22c22b1db0b43518f8f87e1
ztao2 <ztao2@uw.edu>
2023-05-24 18:34:49 -0700

    May 24, Commit 3

---
+++ Client/Board.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import java.io.*;
+import java.util.*;
+
+public class Board extends JPanel{
+  public board() {
+    Dimension size = new Dimension(420, 300);
+    setPreferredSize(size);
+    setMaximumSize(size);
+    setMinimumSize(size);
+
+  }
+  public static void main(String[] args) {
+
+  }
+}
