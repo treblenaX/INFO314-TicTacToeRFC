eca3fd4a70230ce1b948d795feb3100ef670fb9b
iamshivi114 <79740630+iamshivi114@users.noreply.github.com>
2023-05-28 16:35:43 -0700

    Update UDP_Client.py
    Made changes to the UDP Client

--- Client/UDP_Client.py
+++ Client/UDP_Client.py
@@ -1,9 +1,42 @@
 import socket
 
-# 12345678
+msgFromClient = ""
 
+# To start the game
+# Please note if we create a version 2 of our game, we need to change this to add functionality
+def startGame(name):
+    return "HELO 1 "+name
 
-msgFromClient       = "Hello, UDP Server"
+# To get a List of games
+def gameList(filter):
+    if filter == "ALL":
+        return "LIST ALL"
+    return "LIST CURR"
+
+# Create a game
+def createGame(name):
+    return "CREA "+name
+
+# Quit the game
+def quitGame():
+    return "GDBY"
+
+# Join a Game
+def joinGame(gameID):
+    return "JOIN "+gameID
+
+# To make a move
+def move(name, location)
+    return "MOVE "+name+" "+location
+
+# To abandon the game without terminating the session
+def abandonGame():
+    return "QUIT"
+
+# To ask for the status of a game
+def status(game):
+    return "STAT "+game
+
 bytesToSend         = str.encode(msgFromClient)
 serverAddressPort   = ("127.0.0.1", 8000)
 bufferSize          = 1024
